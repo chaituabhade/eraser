bdb4d35521caa5c3c481592ed144a6119e269430
Chaituabhade <cabhade@gmail.com>
2024-07-18 14:40:53 +0530

    pushed

---
+++ eraser/app/_components/Header.tsx
@@ -0,0 +1,68 @@
+
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+
+function Header() {
+  return (
+    <header className="bg-white">
+  <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+    <Image src='/logo.svg' alt='logo'
+    width={100}
+    height={100}
+    />
+
+    <div className="flex flex-1 items-center justify-center md:justify-between">
+      <nav aria-label="Global" className="hidden md:block">
+       
+        <Image src='/logo-1.png' alt='logo'
+              width={50}
+             height={50}
+            />
+      </nav>
+
+      <div className="flex items-center gap-4">
+        <div className="sm:flex sm:gap-4">
+          <div
+            className="block rounded-md  px-5 py-2.5 text-sm font-medium 
+            text-black transition "
+           
+          >
+            <LoginLink postLoginRedirectURL="/dashboard"> Login</LoginLink>
+          </div>
+
+          <div
+            className="hidden rounded-md bg-gray-100 
+            px-5 py-2.5 text-sm font-medium
+             text-black transition
+              hover:text-slate-800 sm:block"
+           
+          >
+          <RegisterLink>Register</RegisterLink>  
+          </div>
+        </div>
+
+        <button
+          className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
+        >
+          <span className="sr-only">Toggle menu</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</header>
+  )
+}
+
+export default Header
